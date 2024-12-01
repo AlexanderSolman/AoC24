ab86061673ac03acc3e1f800e7190d03ed9e864d
AlexanderSolman <alexander.solman.andersson@gmail.com>
2024-12-01 22:35:03 +0100

    setup script for folder structure

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+i=2
+vari="day"
+
+#while(($i < 26)); do
+#    rm -rf $vari$i
+#    i=$((i+1))
+#done
+#
+#i=2
+
+while(($i < 26)); do
+    mkdir $vari$i
+    cd $vari$i
+    go mod init $vari$i
+    cp /home/asolman/Programming/AdventofCode/day1/*.go .
+    cp /home/asolman/Programming/AdventofCode/day1/*.txt .
+    cd ..
+    i=$((i+1))
+done
